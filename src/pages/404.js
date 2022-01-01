4c0f5d100c1ed67fcd05540b1f365e82e6a9647f
Prakhar Dubey <80157067+prakhardubey2002@users.noreply.github.com>
2022-01-01 12:03:43 +0530

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ import Seo from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Your page was not found" />
-    <h1>404: Not Found</h1>
+    <h1>404: Your page was not found </h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
